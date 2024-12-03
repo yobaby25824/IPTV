44f801e2e1a504df09d614a9f3f221e1a7e7d6b9
SH <624089195@qq.com>
2024-12-03 10:18:11 +0800

    Update main.py

--- assets/whitelist-blacklist/main.py
+++ assets/whitelist-blacklist/main.py
@@ -47,7 +47,7 @@ def check_url(url, timeout=6):
     try:
         if url.startswith("http"):
             headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+            'User-Agent': 'okhttp/3.15',
             }
             req = urllib.request.Request(encoded_url, headers=headers)
             with urllib.request.urlopen(req, timeout=timeout) as response:
