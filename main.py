08cedd25fe027ba6f67a1e7e6bc903dc51be3ccd
SH <624089195@qq.com>
2024-12-19 15:03:45 +0800

    Update main.py

--- main.py
+++ main.py
@@ -470,10 +470,8 @@ utc_time = datetime.now(timezone.utc)
 # 北京时间
 beijing_time = utc_time + timedelta(hours=8)
 # 格式化为所需的格式
-formatted_time = beijing_time.strftime("%Y%m%d %H:%M:%S")
-
-about_video="https://gcalic.v.myalicdn.com/gc/wgw05_1/index.m3u8?contentid=2820180516001"
-version=formatted_time+","+about_video
+formatted_time = beijing_time.strftime("%Y%m%d %H:%M")
+version=formatted_time+",https://gcalic.v.myalicdn.com/gc/wgw05_1/index.m3u8?contentid=2820180516001"
 
 # 瘦身版
 all_lines_simple =  ["更新时间,#genre#"] + [version] + ['\n'] +\
