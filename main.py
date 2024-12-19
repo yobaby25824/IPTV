ce2e672df87308698a912c5322b9d4ba56d9be15
SH <624089195@qq.com>
2024-12-19 12:42:30 +0800

    Update main.py

--- main.py
+++ main.py
@@ -268,7 +268,8 @@ def process_channel_line(line):
         channel_name = line.split(',')[0].strip()
         channel_name = traditional_to_simplified(channel_name)  #繁转简
         channel_name = clean_channel_name(channel_name, removal_list)  #分发前清理channel_name中特定字符
-        # channel_name = correct_name_data(channel_name) #根据纠错文件处理
+        print(f"纠错文件: {corrections_name}")
+        channel_name = correct_name_data(channel_name) #根据纠错文件处理
         
         channel_address = clean_url(line.split(',')[1].strip())  #把URL中$之后的内容都去掉
         
