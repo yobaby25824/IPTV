d8db6978e4200215bdb6f0a1dede0b816dbc546e
SH <624089195@qq.com>
2024-12-30 10:33:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -526,7 +526,7 @@ all_lines_simple =  ["更新时间,#genre#"] + [version] + ['\n'] +\
                     ["电视剧频道,#genre#"] + sort_data(dsj_dictionary,dsj_lines) + ['\n'] + \
                     ["综艺频道,#genre#"] + sort_data(zy_dictionary,zy_lines) + ['\n'] + \
                     ["NewTV,#genre#"] + sort_data(newtv_dictionary,newtv_lines) + ['\n'] + \
-                    ["iHOT,#genre#"] + sort_data(ihot_dictionary,ihot_lines)) + ['\n'] + \
+                    ["iHOT,#genre#"] + sort_data(ihot_dictionary,ihot_lines) + ['\n'] + \
                     ["体育频道,#genre#"] + sort_data(ty_dictionary,ty_lines) + ['\n'] + \
                     ["咪咕直播,#genre#"] + sort_data(migu_dictionary,migu_lines)+ ['\n'] + \
                     ["埋堆堆,#genre#"] + sort_data(mdd_dictionary,mdd_lines) + ['\n'] + \
