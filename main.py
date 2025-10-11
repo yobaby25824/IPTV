a5572c862586a21ac65d85e389c305b83871b903
yobaby25824 <wangqi2@live.cn>
2025-10-11 16:15:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -310,17 +310,8 @@ all_lines_simple =  ["更新时间,#genre#"] + [version] + ['\n'] +\
                     ["央视频道,#genre#"] + sort_data(ys_dictionary,ys_lines) + ['\n'] + \
                     ["卫视频道,#genre#"] + sort_data(ws_dictionary,ws_lines) + ['\n'] + \
                     ["港澳台,#genre#"] + sort_data(gat_dictionary,gat_lines) + ['\n'] + \
-                    ["电影频道,#genre#"] + sort_data(ty_dictionary,ty_lines) + ['\n'] + \
-                    ["电视剧频道,#genre#"] + sort_data(ys_dictionary,ys_lines) + ['\n'] + \
-                    ["综艺频道,#genre#"] + sort_data(zy_dictionary,zy_lines) + ['\n'] + \
-                    ["NewTV,#genre#"] + sort_data(newtv_dictionary,newtv_lines) + ['\n'] + \
-                    ["iHOT,#genre#"] + sort_data(ihot_dictionary,ihot_lines) + ['\n'] + \
                     ["体育频道,#genre#"] + sort_data(ty_dictionary,ty_lines) + ['\n'] + \
                     ["咪咕直播,#genre#"] + sort_data(migu_dictionary,migu_lines)+ ['\n'] + \
-                    ["埋堆堆,#genre#"] + sort_data(mdd_dictionary,mdd_lines) + ['\n'] + \
-                    ["音乐频道,#genre#"] + sorted(yy_lines) + ['\n'] + \
-                    ["游戏频道,#genre#"] + sorted(game_lines) + ['\n'] + \
-                    ["解说频道,#genre#"] + sorted(js_lines)
 
 # 合并所有对象中的行文本（去重，排序后拼接）
 all_lines =  all_lines_simple + ['\n'] + \
@@ -446,4 +437,3 @@ print(f"others.txt行数: {other_lines_hj} ")
 #备用1：http://tonkiang.us
 #备用2：https://www.zoomeye.hk,https://www.shodan.io,https://tv.cctv.com/live/
 #备用3：(BlackList检测对象)http,rtmp,p3p,rtp（rtsp，p2p）
-
